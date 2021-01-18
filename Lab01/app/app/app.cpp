9152ab4b62f7d1c29cead421d950ce3694f74000
k0n0di0d4 <72804341+k0n0di0d4@users.noreply.github.com>
2021-01-18 18:04:56 +0100

    Add files via upload

---
+++ Lab01/app/app/app.cpp
@@ -0,0 +1,27 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/videoio.hpp>
+#include <iostream>
+#include <stdint.h>
+
+using namespace cv;
+
+const int fps = 20;
+
+int main(int argc, char** argv)
+{
+	Mat frame;
+	VideoCapture vid(0);
+
+	if (!vid.isOpened()) {
+		return -1;
+	}
+
+	while (vid.read(frame)) {
+		flip(frame, frame, -1);
+		imshow("Webcam", frame);
+		if (waitKey(1000 / fps) == 27)
+			break;
+	}
+	return 0;
+}
